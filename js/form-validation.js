589a7360049db087f95f43521063c90beb544e99
chunpin <james19840628@gmail.com>
2017-05-04 23:48:51 +0200

    yes!almost finished

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
-		// validate signup form on keyup and submit
-	     $("form").validate({
+    // validate signup form on keyup and submit
+       $("form").validate({
           rules: {
             Gender:{ required:true },
             firstname: "required",
@@ -54,46 +54,49 @@ $(document).ready(function() {
           },
           success: function(label) {
             label.text("Eingaben OK").addClass("success");
+          },
+          submitHandler: function() {
+            alert("submitted!");
           }
-		  });
+      });
 
 });
 
 
 
 
-/*		// propose username by combining first- and lastname
-		$("#username").focus(function() {
-			var firstname = $("#firstname").val();
-			var lastname = $("#lastname").val();
-			if (firstname && lastname && !this.value) {
-				this.value = (firstname + "." + lastname).toLowerCase();
-			}
-		});*/
+/*    // propose username by combining first- and lastname
+    $("#username").focus(function() {
+      var firstname = $("#firstname").val();
+      var lastname = $("#lastname").val();
+      if (firstname && lastname && !this.value) {
+        this.value = (firstname + "." + lastname).toLowerCase();
+      }
+    });*/
 
 
 /*
-			// the errorPlacement has to take the table layout into account
-			errorPlacement: function(error, element) {
-				if (element.is(":radio"))
-					error.appendTo(element.parent().next().next());
-				else if (element.is(":checkbox"))
-					error.appendTo(element.next());
-				else
-					error.appendTo(element.parent().next());
-			},
-			// specifying a submitHandler prevents the default submit, good for the demo
-			submitHandler: function() {
-				alert("submitted!");
-			},
-			// set this class to error-labels to indicate valid fields
-			success: function(label) {
-				// set &nbsp; as text for IE
-				label.html("&nbsp;").addClass("checked");
-			},
-			highlight: function(element, errorClass) {
-				$(element).parent().next().find("." + errorClass).removeClass("checked");
-			}
+      // the errorPlacement has to take the table layout into account
+      errorPlacement: function(error, element) {
+        if (element.is(":radio"))
+          error.appendTo(element.parent().next().next());
+        else if (element.is(":checkbox"))
+          error.appendTo(element.next());
+        else
+          error.appendTo(element.parent().next());
+      },
+      // specifying a submitHandler prevents the default submit, good for the demo
+      submitHandler: function() {
+        alert("submitted!");
+      },
+      // set this class to error-labels to indicate valid fields
+      success: function(label) {
+        // set &nbsp; as text for IE
+        label.html("&nbsp;").addClass("checked");
+      },
+      highlight: function(element, errorClass) {
+        $(element).parent().next().find("." + errorClass).removeClass("checked");
+      }
 */
 
 
